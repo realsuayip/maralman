3a7337e48f4259f51fda74f540f1f6011f80aebf
şuayip üzülmez <suayip.541@gmail.com>
2024-02-28 00:01:52 +0300

    Add basic API client

---
+++ src/lib/api-client.js
@@ -0,0 +1,55 @@
+const CLIENT_BASE_URL = process.env.CLIENT_BASE_URL;
+const CLIENT_CREDENTIALS_TOKEN = process.env.CLIENT_CREDENTIALS_TOKEN;
+
+export class Client {
+	constructor(fetch, token) {
+		this.fetch = fetch;
+		this.token = token || CLIENT_CREDENTIALS_TOKEN;
+
+		// Endpoints
+		this.registration = new Registration(this);
+	}
+
+	getURL(endpoint) {
+		return new URL(`api/${endpoint}/`, CLIENT_BASE_URL);
+	}
+
+	getHeaders() {
+		return {
+			Accept: 'application/json',
+			'Content-Type': 'application/json',
+			Authorization: `Bearer ${this.token}`
+		};
+	}
+
+	async post(endpoint, payload) {
+		const method = 'POST';
+		const url = this.getURL(endpoint);
+		const headers = this.getHeaders();
+		const body = JSON.stringify(payload);
+		const options = { method, headers, body };
+		return await this.fetch(url, options);
+	}
+}
+
+class Endpoint {
+	constructor(client) {
+		this.client = client;
+	}
+}
+
+class Verification extends Endpoint {
+	type;
+
+	async send(email) {
+		return await this.client.post(`verifications/${this.type}`, { email });
+	}
+
+	async check(email, code) {
+		return await this.client.post(`verifications/${this.type}/check`, { email, code });
+	}
+}
+
+class Registration extends Verification {
+	type = 'registration';
+}
--- src/routes/register/+page.server.js
+++ src/routes/register/+page.server.js
@@ -1,33 +1,32 @@
+import { Client } from '$lib/api-client.js';
+
 /** @type {import('./$types').Actions} */
 export const actions = {
-	email: async ({ request, fetch, locals }) => {
+	email: async ({ request, fetch }) => {
 		const data = await request.formData();
 		const email = data.get('email');
-		// todo create service layer for api calls
-		await fetch('https://asu.suayip.dev/api/verifications/registration/', {
-			method: 'POST',
-			headers: {
-				'Content-Type': 'application/json',
-				Authorization: `Bearer ${locals.session.get('ident')['access_token']}`
-			},
-			body: JSON.stringify({ email })
-		});
-		return { email, step: 'code' };
+
+		const client = new Client(fetch);
+		const response = await client.registration.send(email);
+		const content = await response.json();
+
+		if (!response.ok) {
+			return { email, step: 'email', errors: content };
+		}
+		return { email: content.email, step: 'code' };
 	},
-	code: async ({ request, fetch, locals }) => {
+	code: async ({ request, fetch }) => {
 		const data = await request.formData();
-		const email = data.get('email');
-		const code = data.get('code');
-		const response = await fetch('https://asu.suayip.dev/api/verifications/registration/check/', {
-			method: 'POST',
-			headers: {
-				'Content-Type': 'application/json',
-				Authorization: `Bearer ${locals.session.get('ident')['access_token']}`
-			},
-			body: JSON.stringify({ email, code })
-		});
-		const { consent } = await response.json();
-		return { email, consent, step: 'user' };
+		const [email, code] = [data.get('email'), data.get('code')];
+
+		const client = new Client(fetch);
+		const response = await client.registration.check(email, code);
+		const content = await response.json();
+
+		if (!response.ok) {
+			return { email, code, step: 'code', errors: content };
+		}
+		return { email, consent: content.consent, step: 'user' };
 	},
 	user: async ({ request }) => {
 		const data = await request.formData();
