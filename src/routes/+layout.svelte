d74a81f94fd7868fa0818121659fc12bec7890aa
şuayip üzülmez <suayip.541@gmail.com>
2024-03-09 00:21:49 +0300

    Add initial design/skeleton

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,6 +4,7 @@
   import Footer from "$lib/components/Footer.svelte";
   import { page } from "$app/stores";
   import { matchRoute } from "$lib/routes.js";
+  import { Home24, Inbox24 } from "svelte-octicons";
 </script>
 
 {#if matchRoute($page.route, "auth") || !$page.data.user.is_authenticated}
@@ -22,10 +23,85 @@
     </div>
   </div>
 {:else if $page.data.user.is_authenticated}
-  <div class="app">
-    <main>
-      <slot />
-    </main>
-    <Footer />
+  <div class="app auth">
+    <nav>
+      <ul>
+        <li><a href="/"><Home24 />Home</a></li>
+        <li><a href="/messages"><Inbox24 />Messages</a></li>
+        <li><a href="/profile">Profile</a></li>
+      </ul>
+      <form action="/logout" method="POST">
+        <button class="btn secondary">Log out</button>
+      </form>
+    </nav>
+    <div class="content">
+      <main>
+        <slot />
+      </main>
+      <aside>
+        <Footer />
+      </aside>
+    </div>
   </div>
 {/if}
+
+<style>
+  .auth {
+    display: flex;
+  }
+
+  .auth > nav {
+    padding: 1rem;
+    position: sticky;
+    top: 0;
+    left: 0;
+    flex-basis: 360px;
+    height: 100vh;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    border-right: 1px solid var(--border-accent);
+    background-color: var(--background);
+  }
+
+  .auth > nav > ul {
+    list-style-type: none;
+    padding: 0;
+    margin: 0;
+  }
+
+  .auth > nav > ul > li > a {
+    padding: 1rem;
+    display: flex;
+    align-items: flex-end;
+    gap: 1.6rem;
+    border-radius: 0.4rem;
+    font-size: 1.75rem;
+    color: var(--foreground);
+    transition: background-color 0.3s ease;
+  }
+
+  .auth > nav > ul > li > a:hover {
+    background: var(--background-accent);
+  }
+
+  .auth > .content {
+    display: flex;
+    width: min(1120px, 100vw);
+  }
+
+  .auth > .content > main {
+    flex-basis: 760px;
+    padding: 1rem 5vh;
+  }
+
+  .auth > .content > aside {
+    padding: 2rem;
+    position: sticky;
+    height: 100vh;
+    top: 0;
+    display: flex;
+    flex-direction: column;
+    gap: 1rem;
+  }
+</style>
