ac3aad35310cc7468b80e291b2293db92b9a2e3a
şuayip üzülmez <suayip.541@gmail.com>
2024-10-24 22:39:10 +0300

    Add auth guard to protected pages

--- src/hooks.server.js
+++ src/hooks.server.js
@@ -1,13 +1,20 @@
 import auth from "$lib/server/auth.js";
+import { matchRoute } from "$lib/routes.js";
+import { redirect } from "@sveltejs/kit";
 
 export async function handle({ event, resolve }) {
-  // todo if failed to auth, 401 incase the page requires auth
-  //  https://github.com/sveltejs/kit/issues/6315#issuecomment-1374680430
-
   const session = new auth.Session(event);
 
   event.locals.session = session;
   event.locals.user = await session.getUser();
+
+  if (
+    !event.locals.user.is_authenticated &&
+    matchRoute(event.route, "protected")
+  ) {
+    // todo: possible to do some ?next=
+    redirect(302, "/");
+  }
 
   session.commit();
   return await resolve(event);
